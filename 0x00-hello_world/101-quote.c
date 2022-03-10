c6242b202479b2c82e7fbea8c27ba0ecff9ce83c
ritakendi <ritabetty77@gmail.com>
2022-03-10 10:06:20 -0800

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * return: 0 (success)
+ * return: Always 1 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
