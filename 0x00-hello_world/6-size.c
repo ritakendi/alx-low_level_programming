52a8cd4946661a7a045b06d61d8a63a644b0b69f
ritakendi <ritabetty77@gmail.com>
2022-03-10 07:07:37 -0800

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  * 
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,4 +12,4 @@ int main(void)
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-	}
+}
