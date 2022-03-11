9392e10dfbc577a3763ffd4e157de8499aad0491
ritakendi <ritabetty77@gmail.com>
2022-03-11 07:31:11 -0800

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -28,7 +28,7 @@ int main(void)
 						putchar(g);
 						putchar(' ');
 						putchar(e);
-						putchat(i);
+						putchar(i);
 						if (h == 57 && g == 56 && e == 57 && i == 57)
 							break;
 						putchar(',');
