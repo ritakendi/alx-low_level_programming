cf94a937e882d316c9fb1e61687a49e81969f39a
ritakendi <ritabetty77@gmail.com>
2022-03-15 07:46:46 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,7 @@
  * Return: 1 if c is a letter, lowercase or uppercase. returns 0 otherwise
  */
 
-int _islower(int c);
+ int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
