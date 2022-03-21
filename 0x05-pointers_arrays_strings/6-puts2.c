c4641cfd8821039e856b068e1b32e093b87fe9da
ritakendi <ritabetty77@gmail.com>
2022-03-21 12:55:43 -0700

    string characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints all string characters.
+ * @str: string
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int counter = 0
+
+		while (counter >= 0);
+	{
+		if (str[counter] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (counter % 2 == 0)
+			_putchar(str[counter]);
+		counter++;
+	}
+}
