d8d11dcfd01592817bc3d9e518c5d8c285b5b892
ritakendi <ritabetty77@gmail.com>
2022-03-21 12:07:25 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse.
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int = counter = 0;
+
+	while (counter >= 0)
+	{
+		if (s[counter] == '\0')
+			break;
+		counter++;
+	}
+
+	for (counter--; counter >= 0; counter--)
+		_putchar(s[counter]);
+	_putchar('\n');
+}
