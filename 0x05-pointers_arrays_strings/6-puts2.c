77bc31b684a8a45c5a273477e493132c0ff6b227
ritakendi <ritabetty77@gmail.com>
2022-03-21 13:07:32 -0700

    string characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,13 @@
 void puts2(char *str)
 {
 	int counter = 0;
-
-		while (counter >= 0);
+	
+	while (counter >= 0)
 	{
 		if (str[counter] == '\0')
 		{
 			_putchar('\n');
-		break;
+			break;
 		}
 		if (counter % 2 == 0)
 			_putchar(str[counter]);
