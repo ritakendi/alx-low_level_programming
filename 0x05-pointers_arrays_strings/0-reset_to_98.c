acd42cbf7a5df5031e82cdeafa150a3180b23d13
ritakendi <ritabetty77@gmail.com>
2022-03-21 10:21:04 -0700

    pointers seem cool

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a ponter to an int and points to 98.
+ * @n: integer value
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Projects aims at learning Arrays, Pointers and Strings in C language.
