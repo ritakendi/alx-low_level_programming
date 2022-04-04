19e20f177cb6c0230415001478d9d26fac49ff17
ritakendi <ritabetty77@gmail.com>
2022-04-04 14:22:37 -0700

    modified the file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * _strup - returns a pointeeeeeer to a newly allocated space in memory.
+ * _strdup - returns a pointeeeeeer to a newly allocated space in memory.
  * @str: string
  *
  * Return: pointer of an array of chars
