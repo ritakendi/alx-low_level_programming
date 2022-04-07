65e5f190be1c93d25fd1c7197f5353047b951909
ritakendi <ritabetty77@gmail.com>
2022-04-07 10:06:39 -0700

    .

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,17 +11,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char c1;
-	char c2
+	char string3;
+	unsigned int i, j, lsout;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	while (*(s1 + c1) != '\0')
-		c1++;
-	if (n >= s2)
-		(s1 + c1) = (s2 + c2)
-	{
-		_putchar("%c", s2);
-	}
-	if (n == NULL)
-		exit(s2);
-	return (s2);
+	for  ( s1 = 0; ! = '\0'; s1++)
+		;
+	for (s2 = 0; != '\0'; s2++)
+		;
+
+	if (n > s2)
+		n = s2;
+	
+	lsout = s1 + n;
+
+	string3 = malloc(lsout + 1);
+
+	if (string3 == NULL)
+		return (NULL);
+
+	for (i = 0; i < lsout; i++)
+		if (i < s1)
+			string3[i] = s2[i - s1];
+
+	string3[i] = '\0';
+	
+	return (string3);
 }
