e5190ad895e49cd782dc891bbcd670b1292a9ed7
ritakendi <ritabetty77@gmail.com>
2022-04-11 14:21:30 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -40,7 +40,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		p_dog->name[i] = name[i];
 	p_dog->name[i] = '\0';
 	p_dog->age = age;
-	
+
 	for (i = 0; i < lowner; i++)
 		p_dog->owner[i] = owner[i];
 	p_dog->owner[i] = '\0';
