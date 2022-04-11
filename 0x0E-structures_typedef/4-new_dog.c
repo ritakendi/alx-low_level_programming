00633e955cf618b78ca2597986d167bd300bbb0a
ritakendi <ritabetty77@gmail.com>
2022-04-11 13:16:59 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,9 +11,9 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p_dog;
-	int j, lname, lowner;
+	int i, lname, lowner;
 
-	p_dog = malloc(sizeof(*store));
+	p_dog = malloc(sizeof(*p_dog));
 	if (p_dog == NULL || !(name) || !(owner))
 	{
 		free(p_dog);
@@ -35,7 +35,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	
-	for (i = 0; i < lnam; i++)
+	for (i = 0; i < lname; i++)
 		p_dog->name[i] = name[i];
 	p_dog->name[i] = '\0';
 
