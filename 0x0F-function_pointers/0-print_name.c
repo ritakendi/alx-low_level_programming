8fe0e17856480694778eefe6d63df0c19d38ba6e
ritakendi <ritabetty77@gmail.com>
2022-04-13 08:07:09 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "fuction_pointers.h"
+
+/**
+ * print_name - calls back to a function
+ * @name: name of person
+ * @f: function pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
