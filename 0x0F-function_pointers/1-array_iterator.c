f01950f288cfeb6bc4a978da87938aa2205790f7
ritakendi <ritabetty77@gmail.com>
2022-04-13 08:30:01 -0700

    iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through an array
+ * performing functions that have been pointed to
+ * @array: the array
+ * @size: array size
+ * @action: pointer to the function
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+		for (i = 0; i < size, i++)
+		{
+			action(array[i]);
+		}
+}
