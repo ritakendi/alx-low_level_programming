87757bc073af03674916a41370bd5881cb0c5bb3
ritakendi <ritabetty77@gmail.com>
2022-04-21 12:40:02 -0700

    list

---
+++ lists.h
@@ -0,0 +1,26 @@
+#ifndef _LISTS_
+#defin _LISTS_
+
+#include <stdioh.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} lisrt_t;
+
+size_t print_list(const list_t *h);
+
+#endif
