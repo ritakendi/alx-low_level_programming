c14b382ec7761d1cb3824c55622e2b9b92e269bf
ritakendi <ritabetty77@gmail.com>
2022-04-22 03:47:53 -0700

    [feature]holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+; Filename: holberton.asm
+; Author: Rita Kendi
+
+global main
+extern printf
+
+section .text
+
+main:
+	mov	rdi, format
+	mov	rax, 0
+	call	printf
+	mov	rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
