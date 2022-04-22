136978c9ef73566a14b221644d789f6519ce60f3
ritakendi <ritabetty77@gmail.com>
2022-04-22 03:33:01 -0700

    [fix]list.h

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,9 +3,9 @@
 /**
  * add_node_end - adds a new node at the end
  * of a list_t list.
- * @head : head of the linked list.
+ * @head: head of the linked list.
  * @str: string to store in the list.
- * Return : address of the head.
+ * Return: address of the head.
  */
 
 list_t *add_node_end(list_t **head, const char *str)
