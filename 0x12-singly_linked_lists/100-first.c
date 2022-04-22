4c9e9de21e17551f9c691f9b53f39a640458eb62
ritakendi <ritabetty77@gmail.com>
2022-04-22 03:38:39 -0700

    [fix]prints

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@
 void __attribute__ ((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n);
+	printf(",\nI bore my house upon my back!\n");
 }
