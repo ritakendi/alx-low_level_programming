4d613e1a881005e068ae6d2c75912f5061198dd8
ritakendi <ritabetty77@gmail.com>
2022-04-28 12:38:19 -0700

    index value

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - return the number of bits you would need 
+ * flip_bits - return the number of bits you would need
  * to flip to get from one number to another.
  * @n: first number
  * @m: second
@@ -11,7 +11,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int nbits;
-	
+
 	for (nbits = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
