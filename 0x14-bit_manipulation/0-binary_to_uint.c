031c10bfcd409256ee1b479f2b75c8ccefa1ddf6
ritakendi <ritabetty77@gmail.com>
2022-04-28 10:57:57 -0700

    unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int len;
 
 	len = strlen(b);
-	for (c = len - 1; c > 0; c--)
+	
+	for (c = len - 1; c >= 0; c--)
 	{
 		if (b[c] != '0' && b[c] != '1')
 			return (0);
