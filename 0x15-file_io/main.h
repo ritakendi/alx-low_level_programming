137958e40cff8fab310d50efa372949f3fcee871
ritakendi <ritabetty77@gmail.com>
2022-05-03 09:30:38 -0700

    oops

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H_
 #include <stdio.h>
 
-int _putchar(void);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
