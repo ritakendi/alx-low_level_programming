b335f5f9ffa5302d14397d8e358747a5f4d26a2a
ritakendi <ritabetty77@gmail.com>
2022-05-03 09:54:46 -0700

    not yet

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,5 +5,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+size_t _strlen(char *);
+void error_file(int file_from, int file_to, char *argv[]);
+int main(int argc, char *argv[]);
+int _putchar(void);
 
 #endif
