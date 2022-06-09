c4e5d96a8ac7c18c9c22885cd10f8b2db6698327
ritakendi <ritabetty77@gmail.com>
2022-06-09 08:01:56 -0700

    counting number of nodes

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: first element of the list.
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h);
+{
+	int count;
+
+	count = 0;
+
+	while (h != Null)
+	{
+		count++;
+		p = p->next;
+	}
+	return(count);
+}
