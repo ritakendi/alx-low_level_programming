74d0f38ebffb5c518b93c079ed6a140a6119864f
ritakendi <ritabetty77@gmail.com>
2022-06-09 10:37:47 -0700

    modified

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,18 +12,19 @@ size_t print_dlistint(const dlistint_t *h)
 	int count;
 
 	count = 0;
+	
+	if (h == NULL)
+		return (count);
 
-	while (h != Null)
-	{
-		count++
-		h = h->next;
-	}
-	return (count);
+	while (h->prev != NULL)
+		h = h->prev;
 
 	while (h != NULL)
 	{
 		print("%d\n", h->n);
+		count++;
 		h = h->next;
+
 	}
 	return (count);
 }
